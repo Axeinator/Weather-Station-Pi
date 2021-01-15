f4843282ca9e00a669cc399712696876c8d4999f
Axeinator <33792363+Axeinator@users.noreply.github.com>
2021-01-15 12:32:20 -0600

    removed api key

--- windyAPI.py
+++ windyAPI.py
@@ -2,7 +2,6 @@ import requests
 
 class Windy:
     key = 'windyAPIKey'
-
     @classmethod
     def upload(cls, temperature, humidity):
         requests.get(f'http://stations.windy.com/update/{key}/?tempf={temperature}')
